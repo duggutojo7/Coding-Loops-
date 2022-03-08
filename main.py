974de610157c48d81e77d141eaaa6d8fb57adff3
duggutojo7 <93871578+duggutojo7@users.noreply.github.com>
2022-03-08 12:27:23 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-#WAP to print fibonacci series.
-
-n=int(input("\nPlease Enter the Range : "))
-i=0
-a=0
-b=1
-while (i < n):
-    if (i <= 1):
-        Next=i
-    else:
-        Next = a + b
-        a = b
-        b = Next
-    print(Next)
-    i = i + 1
+# WAPto print positive numbers in a range.
+list1=[12,-7,5,64,-14]
+list2=[12,14,-95,3]
+for i in list1:
+    if i>0:
+        print(i)
+for j in list2:
+    if j>0:
+        print(j)
